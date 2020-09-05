9f0854b2b181ac9c3071db22ed4cfc370050377a
kiminyeob <kiyeob4416@gmail.com>
2020-09-05 19:32:35 +0900

    Create getHashkeySample.java
    앱의 hash 키 값을 받아올 수 있는 코드이다. JAVA 클래스 파일 내에 아래의 함수를 선언하고 사용하면 된다.
    
    "import android.content.pm.Signature" 를 파일 상단에 복사 붙여 넣기를 한다.

---
+++ getHashkeySample.java
@@ -0,0 +1,20 @@
+private void getHashKey(){
+  PackageInfo packageInfo = null;
+  try {
+      packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+  } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+  }
+  if (packageInfo == null)
+      Log.e("KeyHash", "KeyHash:null");
+
+  for (Signature signature : packageInfo.signatures) {
+      try {
+          MessageDigest md = MessageDigest.getInstance("SHA");
+          md.update(signature.toByteArray());
+          Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+      } catch (NoSuchAlgorithmException e) {
+          Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+      }
+  }
+}
